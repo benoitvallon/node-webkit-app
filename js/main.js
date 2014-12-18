b744023c6908f37033de5e3411982a987d79637e
Benoit VALLON <benoitvallon@gmail.com>
2014-12-18 15:39:57 +0100

    Adds a menu in the window toolbar

--- js/main.js
+++ js/main.js
@@ -25,3 +25,28 @@ document.addEventListener('keyup', function (e) {
     file.save(this.value, document);
   });
 });
+
+var menu = new gui.Menu({ type: 'menubar' });
+
+menu.append(new gui.MenuItem({
+  label: 'File',
+  submenu: new gui.Menu()
+}));
+
+menu.items[0].submenu.append(new gui.MenuItem({
+  label: 'New',
+  click: function () {
+    gui.Window.open('index.html');
+  }
+}));
+menu.items[0].submenu.append(new gui.MenuItem({
+  type: 'separator'
+}));
+menu.items[0].submenu.append(new gui.MenuItem({
+  label: 'Close',
+  click: function () {
+    gui.Window.get().close();
+  }
+}));
+
+gui.Window.get().menu = menu;
