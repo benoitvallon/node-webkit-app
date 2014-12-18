764cd1c714172510a7ed17cb240fe15e968b7d9b
Benoit VALLON <benoitvallon@gmail.com>
2014-12-18 15:26:28 +0100

    Adds the open and save dialogs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 var fs = require('fs');
 var file = require('file.js');
 
-console.log(file.open, file.save);
+function clickInput(id) {
+  var event = document.createEvent('MouseEvents');
+  event.initMouseEvent('click');
+  document.getElementById(id).dispatchEvent(event);
+}
+
+document.addEventListener('keyup', function (e) {
+  if (e.keyCode == 'O'.charCodeAt(0) && e.ctrlKey) {
+    clickInput('open');
+  } else if (e.keyCode == 'S'.charCodeAt(0) && e.ctrlKey) {
+    clickInput('save');
+  }
+});
